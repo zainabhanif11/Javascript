2ef567880fbbfbc75a42bdc2072dde1ec351515d
Zainab Hanif <152397363+zainabhanif11@users.noreply.github.com>
2024-08-26 21:52:52 +0500

    Add files via upload

---
+++ javascript/chapter05/main.js
@@ -0,0 +1,147 @@
+//   Chapter 05
+
+// Q:1 Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+// var num = 3 + 5;
+// document.write("Sum of 3 and 5 is : " + num);
+
+// Q:2 Repeat task1 for subtraction, multiplication, division &
+// modulus.
+// var num = 3 - 5;
+// document.write("Subtraction of 3 and 5 is : " + num + "<br>" );
+
+// var num = 3 % 5;
+// document.write("Multiplication of 3 and 5 is : " + num + "<br>" );
+
+// var num = 3 / 5;
+// document.write("Division of 3 and 5 is : " + num + "<br>" );
+
+// var num = 3 % 5;
+// document.write("Modulus of 3 and 5 is : " + num + "<br>" );
+
+// Q:3 Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value
+
+// MATH EXPRESSIONS | JAVASCRIPT
+
+// Page 2 of 9
+
+// after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”.
+
+// let val;
+// document.write("value after variable declaration is ${val}");
+// val = 4;
+// document.write("intial value: + val +" "+");
+// val ++;
+// document.write("value after addition is  $ {val}");
+// val += 7;
+// document.write("Value after addition is $ {val}");
+// val--;
+// document.write("Value after decrement is $ {val}");
+// let divide = val % 3;
+// document.write('the reminder is :${divide}');
+
+
+// Q:4 Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+// var ticketPrice = 600;
+// var buyingPrice =ticketPrice*5;
+// document.write("Your Total Amount Of Movie Ticket is :" + buyingPrice);
+
+// Q:5Write a script to display multiplication table of any
+// number in your browser. E.g
+// var numberOne = prompt("Enter your number" , );
+// document.write(numberOne + " X " + " 1 = " + numberOne + );
+// document.write(numberOne + " X " + " 2 = " + numberOne * );
+// document.write(numberOne + " X " + " 3 = " + numberOne * );
+// document.write(numberOne + " X " + " 4 = " + numberOne * );
+// document.write(numberOne + " X " + " 5 = " + numberOne * );
+
+
+// Q:8 Store total marks & marks obtained by a student in 2
+// variables. Compute the percentage & show the result in
+// your browser
+
+// var obt_marks =500;
+// var tt_marks = 700;
+// document.write("The Student Percentage is :" + obt_marks/tt_marks*100);
+
+// Q:9 Assume we have 10 US dollars & 25 Saudi Riyals. Write a
+// script to convert the total currency to Pakistani Rupees.
+// Perform all calculations in a single expression.
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee
+// and 1 Saudi Riyal = 28 Pakistani Rupee)
+// var usDollar = 10;
+// var saudiRiyal = 25;
+// var pakistaniRupessDollar = 104.80;
+// var pakistaniRupeesRiyal = 28;
+ 
+// var totalDollar = usDollar * pakistaniRupessDollar;
+// var totalRiyal = saudiRiyal * pakistaniRupeesRiyal;
+// var totalAmount = totalDollar + totalRiyal;
+
+// document.write("Total Currency in PKR : ${totalAmount}");
+
+// Q:10 Write a program to initialize a variable with some
+// number and do arithmetic in following sequence:
+// a. Add 5
+// b. Multiply by 10
+// c. Divide the result by 2
+// Perform all calculations in a single expression
+//  var num = 8;
+//  var Result = ((num + 5) * 10) / 2;
+//  document.write(+ "The result is:" + Result );
+
+// Q:11The Age Calculator: Forgot how old someone is?
+// Calculate it!
+// a. Store the current year in a variable.
+// b. Store their birth year in a variable.
+// c. Calculate their 2 possible ages based on the stored
+// values.
+// Output them to the screen like so: “They are either NN or NN
+
+// var currYear = 2024;
+// var birthYear = 2006;
+// var age = 2024 - 2006;
+
+// document.write("Current year : $ {currYear}");
+// document.write("Birthyear : ${birthYear}");
+// document.write("Age : ${age}");
+
+// Q:12 The Geometrizer: Calculate properties of a circle.
+// a. Store a radius into a variable.
+// MATH EXPRESSIONS | JAVASCRIPT
+// Page 8 of 9
+// b. Calculate the circumference based on the radius, and
+// output “The circumference is NN”.
+// (Hint : Circumference of a circle = 2 π r , π = 3.142)
+// Calculate the area based on the radius, and output “The
+// area is NN”. (Hint : Area of a circle = π r2, π = 3.142)
+
+// var radius = 5;
+// var circumference = 2*3.142 * radius;
+// var area = 3.142 * radius * radius;
+// document.write("The Geometrizer");
+// document.write("The Circumference is" + circumference + );
+// document.write("The area is" + area);
+
+
