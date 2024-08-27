53092c4e5c777ea42445353f1ff6df789ac1e1eb
Zainab Hanif <152397363+zainabhanif11@users.noreply.github.com>
2024-08-27 13:21:09 +0500

    Add files via upload

---
+++ javascript/chapter04/main.js
@@ -0,0 +1,35 @@
+//   chapter # 04
+
+// Q:1 Declare 3 variables in one statement.
+// var username , $myVar , _variable
+
+//  Q:02 Declare 5 legal & 5 illegal variable names.
+
+// // legal
+// var $myVar;
+// var Variable;
+// var _Name;
+// var name123;
+// var myName;
+
+// illegal
+// var 12name;
+// var !exlamination;
+// var 'bahau#aja';
+// var hsjs=nsjs;
+// var /sjsj;
+
+// Ans:3 Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain $myVar, _Name,
+// name123 and myName.
+// For example $my_1stVariable
+// c) Variables must begin with a $name, _name or
+// name123. For example $name, _name or name
+// d) Variable names are case alert
+// e) Variable names should not be JS var
+
+
+
+
+
